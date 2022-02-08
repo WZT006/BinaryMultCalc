562c398f047c113da5e483b15bc157f30d36293a
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2022-02-08 15:25:33 +0800

    Create script.js
    added javascript file

---
+++ script.js
@@ -0,0 +1,9 @@
+
+var calculate = document.getElementById("calculate")
+var multiplicand = document.getElementById("multiplicand")
+var multiplier = document.getElementById("multiplier")
+
+calculate.onclick = function(){
+    console.log(multiplicand.value)
+    console.log(multiplier.value)
+}
