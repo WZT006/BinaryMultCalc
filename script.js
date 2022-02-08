581d92bea7e9c0a6390ab9870c27aad40fb64eba
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2022-02-08 19:40:31 +0800

    added number validation for binary

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ var multiplicand = document.getElementById("multiplicand")
 var multiplier = document.getElementById("multiplier")
 
 calculate.onclick = function(){
-    console.log(multiplicand.value)
-    console.log(multiplier.value)
+    
+    // number validation for binary
+    if(document.getElementById('binaryToggle').checked){
+        var num1 = multiplicand.value
+        var num2 = multiplier.value
+        while (num1) {
+            if (num1 % 10 > 2){
+                window.alert("Invalid Value for Multiplicand")
+                return false
+            }
+            num1 = Math.floor(num1 / 10);
+        }
+
+        while (num2) {
+            if (num2 % 10 > 2){
+                window.alert("Invalid Value for Multiplier")
+                return false
+            }
+            num2 = Math.floor(num2 / 10);
+        }
+    } 
 }
